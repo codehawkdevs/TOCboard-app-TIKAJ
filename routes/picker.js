f4d1ae34e73592d5b91e543171829fa226f04b13
Harsh Sahu <harsh.sahu@outlook.com>
2019-07-13 16:39:27 +0530

    [FIX ] now file folder ID can be added under the profile section

--- routes/picker.js
+++ routes/picker.js
@@ -3,7 +3,7 @@ const User = require("../models/user");
 
 router.get("/", (req, res) => {
   if (req.user) {
-    res.render("picker", { user: req.user });
+    res.render("picker");
   } else {
     res.redirect("/auth/login");
   }
@@ -11,12 +11,14 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   // you have address available in req.body:
+
+  let email = req.user._id;
   const fileFolderId = req.body.file;
   User.findOne({ _id: email }).then(currentUser => {
     if (currentUser) {
       currentUser.fileFolderIDs.push(fileFolderId);
       currentUser.save();
-      return res.send("File/Folder ID Added Successfully");
+      return res.send("Tag Added Successfully");
     } else {
       return res.send("User not found");
     }
